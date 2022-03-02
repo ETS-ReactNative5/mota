ee00609875696d9c3e43dc54a4b0fa17e1623cf5
Organic Hemp Mexico <92967369+organicghemp@users.noreply.github.com>
2022-03-02 04:27:10 -0800

    Initialize treemap-url-encode-gzip

---
+++ lighthouse-treemap/types/treemap.d.ts
@@ -0,0 +1,51 @@
+import _TreemapUtil = require('../app/src/util.js');
+
+export type Strings = Record<LH.Locale, import('../../lighthouse-core/lib/i18n/locales').LhlMessages>;
+
+declare global {
+  class WebTreeMap {
+    constructor(data: any, options: WebTreeMapOptions);
+    render(el: HTMLElement): void;
+    layout(data: any, el: HTMLElement): void;
+    zoom(address: number[]): void
+  }
+
+  interface WebTreeMapOptions {
+    padding: [number, number, number, number];
+    spacing: number;
+    caption(node: LH.Treemap.Node): string;
+    showNode?(node: LH.Treemap.Node): boolean;
+  }
+
+  interface RenderState {
+    root: LH.Treemap.Node;
+    viewMode: LH.Treemap.ViewMode;
+  }
+
+  interface NodeWithElement extends LH.Treemap.Node {
+    /** webtreemap adds dom to node data. */
+    dom?: HTMLElement;
+  }
+
+  var webtreemap: {
+    TreeMap: typeof WebTreeMap;
+    render(el: HTMLElement, data: any, options: WebTreeMapOptions): void;
+    sort(data: any): void;
+  };
+  var TreemapUtil: typeof _TreemapUtil;
+  var strings: Strings;
+
+  interface DecompressionStream extends GenericTransformStream {
+    readonly format: string;
+  }
+  const DecompressionStream: {
+    prototype: DecompressionStream,
+    new (format: string): DecompressionStream,
+  };
+  interface Window {
+    __treemapOptions?: LH.Treemap.Options;
+
+  }
+}
+
+export {};
