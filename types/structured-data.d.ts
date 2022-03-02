b55bdd111c1dcb1e3be3e82456cb01326d94224b
Organic Hemp Mexico <92967369+organicghemp@users.noreply.github.com>
2022-03-02 04:14:37 -0800

    Initialize structured-data-automatic-2

---
+++ types/structured-data.d.ts
@@ -0,0 +1,54 @@
+/**
+ * @license Copyright 2018 The Lighthouse Authors. All Rights Reserved.
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License. You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
+ */
+
+declare global {
+  module LH {
+    module StructuredData {
+
+      export type ValidatorType = "json" | "json-ld" | "json-ld-expand" | "schema-org";
+    
+      export interface ValidationError {
+        message: string;
+        /** Property path in the expanded JSON-LD object */
+        path?: string;
+        validator: ValidatorType;
+        lineNumber?: number | null;
+        /** Schema.org type URIs of the invalid entity (undefined if type is invalid) */
+        validTypes?: Array<{
+          uri: string;
+          name: string;
+        }>;
+      }
+
+      export type JsonLDPrimitive = string | number | boolean | undefined
+      export type JsonLDPropertyValue = JsonLDPrimitive | JsonLDPrimitive[] | JsonLDDocument
+
+      export type JsonLDDocument = {
+        '@type'?: string,
+        "name"?: JsonLDPropertyValue,
+        [property: string]: JsonLDPropertyValue
+      }
+
+      export interface ExpandedSchemaRepresentationItem {
+        [schemaRef: string]: Array<
+            boolean | string |  ExpandedSchemaRepresentationItem |
+            {
+              '@id'?: string;
+              '@type'?: string;
+              '@value'?: JsonLDPrimitive
+            }
+          >;
+      }
+
+      export type ExpandedSchemaRepresentation =
+        | Array<ExpandedSchemaRepresentationItem>
+        | ExpandedSchemaRepresentationItem;
+    }
+  }
+}
+
+// empty export to keep file a module
+export {};
