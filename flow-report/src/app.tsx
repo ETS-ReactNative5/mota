48a0955e9c9b1cfccefa52f649279f54530c35ce
Organic Hemp Mexico <92967369+organicghemp@users.noreply.github.com>
2022-03-02 04:33:40 -0800

    Initialize nosniff

---
+++ flow-report/src/app.tsx
@@ -0,0 +1,44 @@
+/**
+ * @license Copyright 2021 The Lighthouse Authors. All Rights Reserved.
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License. You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
+ */
+
+import {FunctionComponent} from 'preact';
+import {Sidebar} from './sidebar/sidebar';
+import {FlowResultContext, useCurrentLhr} from './util';
+
+const Report: FunctionComponent<{lhr: LH.Result}> = ({lhr}) => {
+  // TODO(FR-COMPAT): Render an actual report here.
+  return (
+    <div data-testid="Report">
+      <h1>{lhr.finalUrl}</h1>
+      {
+        Object.values(lhr.categories).map((category) =>
+          <h2 key={category.id}>{category.id}: {category.score}</h2>
+        )
+      }
+    </div>
+  );
+};
+
+const Summary: FunctionComponent = () => {
+  // TODO(FR-COMPAT): Design summary page.
+  return <h1 data-testid="Summary">SUMMARY</h1>;
+};
+
+const Content: FunctionComponent = () => {
+  const currentLhr = useCurrentLhr();
+  return currentLhr ? <Report lhr={currentLhr.value}/> : <Summary/>;
+};
+
+export const App: FunctionComponent<{flowResult: LH.FlowResult}> = ({flowResult}) => {
+  return (
+    <FlowResultContext.Provider value={flowResult}>
+      <div className="App">
+        <Sidebar/>
+        <Content/>
+      </div>
+    </FlowResultContext.Provider>
+  );
+};
---
+++ flow-report/types/flow-report.d.ts
@@ -0,0 +1,26 @@
+/**
+ * @license Copyright 2021 The Lighthouse Authors. All Rights Reserved.
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License. You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
+ */
+
+import FlowResult_ from '../../types/lhr/flow';
+import LHResult from '../../types/lhr/lhr';
+import * as Settings from '../../types/lhr/settings';
+
+declare global {
+  interface Window {
+    __LIGHTHOUSE_FLOW_JSON__: FlowResult_;
+    __initLighthouseFlowReport__: () => void;
+  }
+
+  // Expose global types in LH namespace.
+  module LH {
+    export import Result = LHResult;
+    export type FlowResult = FlowResult_;
+    export type Locale = Settings.Locale;
+    export type ConfigSettings = Settings.ConfigSettings;
+  }
+}
+
+export {};
