cbaa62d63f8dc3370c21e2b7612b5a557f045bb0
Organic Hemp Mexico <92967369+organicghemp@users.noreply.github.com>
2022-03-02 04:08:10 -0800

    Initialize stryker

---
+++ stryker.conf.js
@@ -0,0 +1,29 @@
+'use strict';
+
+const JEST_WORKERS = 4;
+const STRYKER_WORKERS = 2;
+
+module.exports = function(config) {
+  config.set({
+    mutator: 'javascript',
+    packageManager: 'yarn',
+    testRunner: 'command',
+    commandRunner: {
+      command:
+          'yarn tsc -p . && ' +
+          `yarn unit-core --maxWorkers=${JEST_WORKERS} --bail`,
+    },
+    transpilers: [],
+    coverageAnalysis: 'off',
+    mutate: [
+      'lighthouse-core/computed/**/*.js',
+    ],
+    reporters: ['clear-text', 'progress', 'html'],
+    htmlReporter: {
+      baseDir: 'coverage/mutation/html',
+    },
+    // logLevel: 'all',
+    timeoutMS: 80000,
+    maxConcurrentTestRunners: STRYKER_WORKERS,
+  });
+};
